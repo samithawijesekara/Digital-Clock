c0ca6c08a772b7e1e6bc7500218120b5fd6fb4fc
chathuralalinda <chathura9710@gmail.com>
2020-11-20 12:14:48 +0530

    Digital Clock V.1 added

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+setInterval (updateDigitalClock , 1000);
+
+function updateDigitalClock() {
+
+	var updateDate = new Date();
+	var updateHours = updateDate.getHours();
+	var updateMinutes = updateDate.getMinutes();
+	var updateSeconds = updateDate.getSeconds();
+
+	if(updateHours < 10) {updateHours = "0" + updateHours};
+	if(updateMinutes < 10) {updateMinutes = "0" + updateMinutes};
+	if(updateSeconds < 10) {updateSeconds = "0" + updateSeconds};
+
+	var updateClock = updateHours + ":" + updateMinutes + ":" + updateSeconds
+
+	document.getElementById("DigitalClock").innerText = updateClock;
+}
