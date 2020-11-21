1de7c4a0e8e5a78cf837a5960d12be1ff2992f95
samithawijesekara@gmail.com <samithawijesekara@gmail.com>
2020-11-21 13:02:13 +0530

    Change some styles & add greeting text

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-
-setInterval (updateDigitalClock , 1000);
+setInterval(updateDigitalClock, 1000);
 
 function updateDigitalClock() {
 
-	var updateDate = new Date();
-	var updateHours = updateDate.getHours();
-	var updateMinutes = updateDate.getMinutes();
-	var updateSeconds = updateDate.getSeconds();
+    var updateDate = new Date();
+    var updateHours = updateDate.getHours();
+    var updateMinutes = updateDate.getMinutes();
+    var updateSeconds = updateDate.getSeconds();
 
-	if(updateHours < 10) {updateHours = "0" + updateHours};
-	if(updateMinutes < 10) {updateMinutes = "0" + updateMinutes};
-	if(updateSeconds < 10) {updateSeconds = "0" + updateSeconds};
+    if (updateHours < 10) { updateHours = "0" + updateHours };
+    if (updateMinutes < 10) { updateMinutes = "0" + updateMinutes };
+    if (updateSeconds < 10) { updateSeconds = "0" + updateSeconds };
 
-	var updateClock = updateHours + ":" + updateMinutes + ":" + updateSeconds
+    var updateClock = updateHours + ":" + updateMinutes + ":" + updateSeconds
 
-	document.getElementById("DigitalClock").innerText = updateClock;
+    document.getElementById("DigitalClock").innerText = updateClock;
+
+    if (updateHours <= 12) {
+        document.getElementById("greeting").innerHTML = "Good Mornig.!!";
+    } else {
+        document.getElementById("greeting").innerHTML = "Good Afternon.!!";
+    }
 }
